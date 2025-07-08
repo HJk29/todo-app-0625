c0c66361ee8c4e19f195c49b2ee9c41a1ca331f0
HJK29 <rlawnsgk22@gmail.com>
2025-07-08 16:58:19 +0900

    커스텀 헤더 구현

--- App.js
+++ App.js
@@ -1,14 +1,32 @@
-import { StyleSheet } from 'react-native';
+import { StyleSheet, View, Text, StatusBar, Dimensions } from "react-native";
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 import React from "react";
-import tabConfig from './configs/tabConfig.js'; 
+import tabConfig from './configs/tabConfig.js';
 import { TodosProvider } from './components/TodosProvider.js';
+import { SafeAreaView } from 'react-native-safe-area-context';
+
+const { width, height } = Dimensions.get("window");
+
+const CustomHeader = ({ title }) => {
+  return (
+    <>
+      {/* 개발할 때 SafeAreaView는 필수 */}
+      <SafeAreaView>
+        <StatusBar barStyle="dark-content" backgroundColor="#fff" />
+        <View style={styles.headerBox}>
+          <Text style={styles.headerTitle}>{title}</Text>
+        </View>
+      </SafeAreaView>
+    </>
+  )
+}
 
 // 네비게이션 스택
 const Stack = createNativeStackNavigator();
 const Tab = createBottomTabNavigator();
+
 
 
 // 앱 컴포넌트
@@ -53,7 +71,10 @@ export default function App() {
               key={routeConfig.name}
               name={routeConfig.name}
               component={routeConfig.component}
-              options={{ title: routeConfig.title }}
+              options={{
+                title: routeConfig.title,
+                header: () => <CustomHeader title={routeConfig.title} />
+              }}
             />
           ))}
         </Tab.Navigator>
@@ -62,3 +83,22 @@ export default function App() {
 
   );
 }
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  headerBox: {
+    height: height * 0.05,
+    backgroundColor: "#fff",
+    justifyContent: "center",
+    alignItems: "flex-end",
+  },
+  headerTitle: {
+    fontSize: 25,
+    fontWeight: "bold",
+    marginRight: 15,
+  },
+});

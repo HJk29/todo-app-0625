d57fc27e53d5ae425462fbce8a72536c96269978
HJK29 <rlawnsgk22@gmail.com>
2025-07-08 15:13:26 +0900

    테스트 데이터를 별도의 컴포넌트로 분리

---
+++ components/TestTodosData.js
@@ -0,0 +1,60 @@
+import { dateToStr } from "../utils/util"
+
+const testTodosData = [
+    {
+      id: 1,
+      content: "테스트 치기",
+      regDate: dateToStr(new Date())
+    },
+    {
+      id: 2,
+      content: "리액트 공부",
+      regDate: dateToStr(new Date())
+    },
+    {
+      id: 3,
+      content: "밥 먹기",
+      regDate: dateToStr(new Date())
+    },
+    {
+      id: 4,
+      content: "테니스 치기",
+      regDate: dateToStr(new Date()),
+    },
+    {
+      id: 5,
+      content: "테니스 치기",
+      regDate: dateToStr(new Date()),
+    },
+    {
+      id: 6,
+      content: "테니스 치기",
+      regDate: dateToStr(new Date()),
+    },
+    {
+      id: 7,
+      content: "테니스 치기",
+      regDate: dateToStr(new Date()),
+    },
+    {
+      id: 8,
+      content: "테니스 치기",
+      regDate: dateToStr(new Date()),
+    },
+    {
+      id: 9,
+      content: "테니스 치기",
+      regDate: dateToStr(new Date()),
+    },
+    {
+      id: 10,
+      content: "테니스 치기",
+      regDate: dateToStr(new Date()),
+    },
+    {
+      id: 11,
+      content: "테니스 치기",
+      regDate: dateToStr(new Date()),
+    },
+  ];
+  export default testTodosData;
--- components/TodosProvider.js
+++ components/TodosProvider.js
@@ -1,74 +1,17 @@
 import { dateToStr } from "../utils/util";
 import React, { createContext, useState, useRef } from "react";
+import testTodosData from "./TestTodosData";
 
 const TodosContext = createContext();
 
 export const TodosProvider = ({ children }) => {
 
 
-  const testTodo = [
-    {
-      id: 1,
-      content: "테스트 치기",
-      regDate: dateToStr(new Date())
-    },
-    {
-      id: 2,
-      content: "리액트 공부",
-      regDate: dateToStr(new Date())
-    },
-    {
-      id: 3,
-      content: "밥 먹기",
-      regDate: dateToStr(new Date())
-    },
-    {
-      id: 4,
-      content: "테니스 치기",
-      regDate: dateToStr(new Date()),
-    },
-    {
-      id: 5,
-      content: "테니스 치기",
-      regDate: dateToStr(new Date()),
-    },
-    {
-      id: 6,
-      content: "테니스 치기",
-      regDate: dateToStr(new Date()),
-    },
-    {
-      id: 7,
-      content: "테니스 치기",
-      regDate: dateToStr(new Date()),
-    },
-    {
-      id: 8,
-      content: "테니스 치기",
-      regDate: dateToStr(new Date()),
-    },
-    {
-      id: 9,
-      content: "테니스 치기",
-      regDate: dateToStr(new Date()),
-    },
-    {
-      id: 10,
-      content: "테니스 치기",
-      regDate: dateToStr(new Date()),
-    },
-    {
-      id: 11,
-      content: "테니스 치기",
-      regDate: dateToStr(new Date()),
-    },
-  ]
-
   // const [todos, setTodos] = useState([]);
   // const lastTodoIdRef = useRef(0);
   
-  const [todos, setTodos] = useState([...testTodo]);
-  const lastTodoIdRef = useRef(testTodo.length);
+  const [todos, setTodos] = useState([...testTodosData]);
+  const lastTodoIdRef = useRef(testTodosData.length);
 
   const addTodo = (newContent) => {
     const id = ++lastTodoIdRef.current;
